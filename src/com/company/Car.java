0adfac8654b5bfeab1cabc4dadb93040e7c24b9f
AVolnov <DEDAN3209@yandex.ru>
2018-12-25 00:14:07 +0300

    Merge branch 'develop'
    # Conflicts:
    #	src/com/company/Main.java

---
+++ src/com/company/Car.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Car {
+    public String mark;
+
+    public String weight;
+
+    public String power;
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        this.power = power;
+    }
+}
